2d16f676e0abe53fdeed212ce0be8ebdf3acd667
Leonard <leonard.flachs@gmail.com>
2020-02-02 19:14:38 +0100

    Updates

---
+++ precache-manifest.ce2b6e7339344ef7286dcb487b192338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06377e072dbb9a6d44ff721286c28391",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eed8c507afa873c0bf65",
+    "url": "./static/css/main.9dbcb0e5.chunk.css"
+  },
+  {
+    "revision": "8d90fa7fe96e055519ab",
+    "url": "./static/js/2.cf526805.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.cf526805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed8c507afa873c0bf65",
+    "url": "./static/js/main.f6fe04c3.chunk.js"
+  },
+  {
+    "revision": "4e08e05a4799a67b4a2f",
+    "url": "./static/js/runtime-main.3513734e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
