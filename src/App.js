0804fbb37653682f4296ea613b220f41564d6825
Leonard <leonard.flachs@gmail.com>
2020-02-17 20:49:57 +0100

    add the search bar

--- src/App.js
+++ src/App.js
@@ -1,48 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './component/Navbar';
-import MediaCard from './component/MediaCard';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import MediaCard from "./component/MediaCard";
 
 const houseToRent = [
   {
     name: "Tiny House refuge",
     desc: "This is the perfect house for you, come to visit it you'll love it ",
-    img: "https://cdn.wohnglueck.de/vary/content/2019/09/Tiny-House-Resort-Tinyhousedesign_content-1400x788@2x.jpg",
+    img:
+      "https://cdn.wohnglueck.de/vary/content/2019/09/Tiny-House-Resort-Tinyhousedesign_content-1400x788@2x.jpg",
     available: true
   },
   {
     name: "Beautiful design house",
     desc: "This is the perfect house for you, come to visit it you'll love it ",
-    img: "https://images.adsttc.com/media/images/5de3/1ca6/3312/fda8/2a00/00b3/newsletter/001.jpg?1575165037",
+    img:
+      "https://images.adsttc.com/media/images/5de3/1ca6/3312/fda8/2a00/00b3/newsletter/001.jpg?1575165037",
     available: true
   },
   {
     name: "Beautiful House ",
     desc: "This is the perfect house for you, come to visit it you'll love it ",
-    img: "https://images.adsttc.com/media/images/5be9/fd5c/08a5/e5a5/8c00/008f/newsletter/CARLES_FAUS_ARQUITECTURA_-_CARMEN_HOUSE_(2).jpg?1542061390",
+    img:
+      "https://images.adsttc.com/media/images/5be9/fd5c/08a5/e5a5/8c00/008f/newsletter/CARLES_FAUS_ARQUITECTURA_-_CARMEN_HOUSE_(2).jpg?1542061390",
     available: true
   },
   {
     name: "My super house ",
     desc: "This is the perfect house for you, come to visit it you'll love it ",
-    img: "https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale",
+    img:
+      "https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale",
     available: true
-  },
-]
+  }
+];
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="cards">
-        {houseToRent.map((element) => {
-          return (<MediaCard desc={element.desc} title={element.name} img={element.img} />)
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      houses: houseToRent
+    };
+  }
+  onSearchChange = event => {
+    let newHouses = houseToRent.filter(element => {
+      return element.name.includes(event.target.value);
+    });
 
-        })}
+    this.setState({
+      houses: newHouses
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar onSearchChange={this.onSearchChange} />
+        <div className="cards">
+          {this.state.houses.map(element => {
+            return (
+              <MediaCard
+                desc={element.desc}
+                title={element.name}
+                img={element.img}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/component/MediaCard.js
+++ src/component/MediaCard.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
-    card: {
-        maxWidth: 345,
-        margin: "1em"
-    },
-    media: {
-        height: 140,
-    },
+  card: {
+    maxWidth: 345,
+    margin: "1em"
+  },
+  media: {
+    height: 140
+  }
 });
 
 function MediaCard(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.card}>
-            <CardActionArea>
-                <CardMedia
-                    className={classes.media}
-                    image={props.img}
-                    title={props.title}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.desc}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Share
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.img}
+          title={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.desc}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
         </Button>
-                <Button size="small" color="primary">
-                    Learn More
+        <Button size="small" color="primary">
+          Learn More
         </Button>
-            </CardActions>
-        </Card>
-    );
+      </CardActions>
+    </Card>
+  );
 }
 export default MediaCard;
